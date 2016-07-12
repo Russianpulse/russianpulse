56973438987812f960aa3b960b8cc877e773522e
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-07-12 19:47:27 +0300

    cheap bye controller

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -2,26 +2,7 @@ class RedirectsController < ApplicationController
   def bye
     @url = params[:url]
 
-    @article = Readability::Document.new(page_html, attributes: %w[src href], remove_empty_nodes: false)
-
-    @image = article_image
-
     days = params[:days] || 7
     @posts = Post.newer_than(days.to_i.days.ago).order("views DESC").limit(20)
   end
-
-  private
-
-  def page_html
-    Rails.cache.fetch("goto##{@url}") do
-      open(@url).read
-    end
-  end
-
-  def article_image
-    Rails.cache.fetch("goto##{@url}#image") do
-      @article.images.first
-    end
-  end
-
 end
