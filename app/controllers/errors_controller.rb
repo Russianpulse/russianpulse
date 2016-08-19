acfcd254125cee04bca11017208112eff9b65c79
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-08-19 10:03:12 +0300

    not found page: no template error fixed

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,9 @@
 class ErrorsController < ApplicationController
   def not_found
+    respond_to do |format|
+      format.html { render 'errors/not_found', status: 404 }
+      format.any { render text: 'not found', status: 404 }
+    end
   end
 
   def exception
