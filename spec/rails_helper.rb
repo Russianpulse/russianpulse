471f5e3320a910eb16d2b0440a2eb4c5f667a723
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-08-22 10:06:57 +0300

    devise test helper updated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
