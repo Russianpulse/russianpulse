8d85430fa269204107c0c7875e627d5b469eb5e3
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-08-30 20:07:18 +0300

    cache archive

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,10 +1,13 @@
 class ArchiveController < ApplicationController
   def index
+    expires_in(12.hours, public: true)
   end
 
   def day
     @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
 
     @posts = Post.created_between(@date.beginning_of_day, @date.end_of_day).recent
+    
+    expires_in(1.week, public: true) if @date < 1.day.ago
   end
 end
