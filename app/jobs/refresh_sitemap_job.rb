c161471db529bad530df49c02ebcb106ffe6cf47
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-02-28 22:24:40 +0300

    notify when sitemap generation started

--- app/jobs/refresh_sitemap_job.rb
+++ app/jobs/refresh_sitemap_job.rb
@@ -2,8 +2,14 @@ class RefreshSitemapJob < ActiveJob::Base
   queue_as :default
 
   def perform(*_args)
+    EventTracker.notify :sitemap, :started
     SitemapGenerator::Interpreter.run(config_file: nil, verbose: false)
     SitemapGenerator::Sitemap.ping_search_engines
     EventTracker.track_and_notify :sitemap, :refresh
+
+  rescue StandardError => ex
+    EventTracker.notify :sitemap, :failed, ex.to_s
+
+    raise ex
   end
 end
