9027e0fdb5e418c0a94a45e3841472245b97a293
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-05-18 08:39:15 +0300

    log assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   config.assets.enabled = true
 
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "//#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com"
 
