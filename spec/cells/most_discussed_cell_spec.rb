c3c8692c7ca6208ee58d7b952aa3ba62e7a872a0
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-06-15 10:47:00 +0300

    most discussed by comments

---
+++ spec/cells/most_discussed_cell_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe MostDiscussed, type: :cell do
+  include Cell::Testing
+  controller WelcomeController
+  
+  let!(:current_post) { create :post, :commented }
+  let!(:post) { create :post, :commented }
+  let!(:post_without_comments) { create :post }
+  before do
+    5.times do
+      create :post, :commented
+    end
+  end
+
+  subject { cell('most_discussed', current_post).call(:show) }
+
+  it { is_expected.to include post.title }
+  it { is_expected.not_to include post_without_comments.title }
+  it { is_expected.not_to include current_post.title }
+end
