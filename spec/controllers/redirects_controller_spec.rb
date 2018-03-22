37c3e03b786f14174d0695958991efcf1be1ddbf
Sergei O. Udalov <sergei.udalov@gmail.com>
2018-03-22 08:54:55 +0300

    bye test fixed

--- spec/controllers/redirects_controller_spec.rb
+++ spec/controllers/redirects_controller_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe RedirectsController, type: :controller do
   describe 'GET #bye' do
     it 'returns http success' do
-      get :bye, params: { url: 'http://russianpulse.ru/colonelcassad/2016/02/21/1500394-krym-problemy-bezopasnosti' }
+      get :bye, params: { url: 'https://goo.gl/ubUBub' }
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http redirect' do
+      get :bye, params: { url: 'http://russianpulse.ru/colonelcassad/2016/02/21/1500394-krym-problemy-bezopasnosti' }
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
