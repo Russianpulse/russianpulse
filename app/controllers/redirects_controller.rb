973643b343ae6ca20717dd745c536f830fc14e8b
Sergei O. Udalov <sergei.udalov@gmail.com>
2018-03-22 08:32:10 +0300

    redirect to root unless goo.gl

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -2,8 +2,12 @@ class RedirectsController < ApplicationController
   def bye
     @url = params[:url]
 
-    days = params[:days] || 7
-    @posts = Post.newer_than(days.to_i.days.ago).order('views DESC').limit(20)
+    if @url.match /goo\.gl/
+      days = params[:days] || 7
+      @posts = Post.newer_than(days.to_i.days.ago).order('views DESC').limit(20)
+    else
+      redirect_to root_path
+    end
 
     expires_in(30.minutes, public: true)
   end
