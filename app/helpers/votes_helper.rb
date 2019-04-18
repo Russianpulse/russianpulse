7570c5818e67f4aac74faa85f9125de06a3212f6
Sergei O. Udalov <sergei.udalov@gmail.com>
2019-04-19 00:47:34 +0300

    comment vote style

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,9 +1,9 @@
 module VotesHelper
   def upvote_button_class(item)
-    'btn-success' if signed_in? && current_user.voted_up_on?(item)
+    'btn-info' if signed_in? && current_user.voted_up_on?(item)
   end
 
   def downvote_button_class(item)
-    'btn-danger' if signed_in? && current_user.voted_down_on?(item)
+    'btn-info' if signed_in? && current_user.voted_down_on?(item)
   end
 end
